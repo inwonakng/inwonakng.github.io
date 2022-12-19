1802c34d9b6139c294c4c0a8de0abd4dbe8647ab
inwonakng <inwon.kang04@gmail.com>
2022-12-19 12:05:12 -0500

    he move good

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,18 +1,37 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { RouteType } from '../routes';
+import shinChanGif from '../shinchan.gif'
+import shinChanStatic from '../shinchan.png'
 
 type MenuBarProps = {
     routes: RouteType[];
 }
+
+const Logo = () => {
+    const [isHover,setHover] = useState(false)
+
+    
+    return(
+        <img 
+            src={isHover ? shinChanGif : shinChanStatic} 
+            style={{height:'5vh'}}
+            onMouseEnter={() => setHover(!isHover)}
+            onMouseLeave={() => setHover(!isHover)}
+        />
+    )
+}
+
+
 
 const MenuBar = ({routes}:MenuBarProps) => {
     return(
     <div className='menubar'>
         <Navbar bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="/">Inwon Kang</Navbar.Brand>
+                <Navbar.Brand href="#/home"><Logo/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,55 +2,71 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 import headshot from '../hawaii.jpg'
+import { useState } from 'react'
 
-const Home = () => 
-<>
-    <Row>
-        <Col xs={3}>
-            <img src={headshot} style={{width:'100%'}}/>
-        </Col>
-        <Col xs={9}>
-            <Row>
-                <h3>Inwon Kang</h3> 
-                <p>
-                Email: <a href="mailto:inwon.kang04@gmail.com" target="_blank" rel="noreferrer">inwon.kang04@gmail.com</a>
-                </p>
-            </Row>
-            <Row>
-                <Col>
-                I am a current PhD student at Rensselaer Polytechnic Institute.
-                I work mostly with Python for data/analysis oriented projects. I am also familiar with web frameworks in javascript and python for buliding quick web pages.
-                <br/>
-                <br/>
-                During my undergraduate and master's program, I focused on explaininable machine learning using shallow models. 
-                My master's project was on collecting survey data from participants and building explainable ML models to try to explain the results. 
-                During this time, I also dabbled in NLP and built an enhanced pipeline of existing state-of-art model for <em>Preference Ellicitation</em> task using GNNs and coreference parsers.
-                <br/>
-                <br/>
-                My PhD focus will most likely be on blockchains and integration of machine learning with blockchains.
-                <br/>
-                <br/>
-                I like collecting datasets, one day one of them will be useful.
-                </Col>
-                <Col xs={12} className='mt-5'>
-                    <h5>Education</h5>
-                </Col>
-                <Col xs={12}>
-                    <ul>
-                        <li>
-                            BS in Computer Science with concentration in AI/ML (Aug 2017 - May 2021). <b>RPI</b>
-                        </li>
-                        <li>
-                            MS in Computer Science (Aug 2021 - May 2022). Supervised by Prof. <a href="https://www.cs.rpi.edu/~xial/" target="_blank" rel="noreferrer">Lirong Xia</a> <b>RPI</b>
-                        </li>
-                        <li>
-                            PhD in Computer Science (Aug 2022 - ). Supervised by Prof. <a href="https://faculty.rpi.edu/oshani-seneviratne" target="_blank" rel="noreferrer">Oshani Seneviratne</a> <b>RPI</b>
-                        </li>
-                    </ul>
-                </Col>
-            </Row>
-        </Col>
-    </Row>
-</>
+const Home = () => {
+
+    const [width, setWidth] = useState<number>(window.innerWidth);
+
+    return(
+    <>
+        <Row>
+        {
+            width < 1000 ? null
+            :<Col xs={3}>
+                <img src={headshot} style={{width:'100%'}}/>
+            </Col>
+        }
+            <Col xs={width < 1000 ? 12: 9}>
+            {
+                width < 1000
+                ?<Row className='mb-3'>
+                    <img src={headshot} style={{width:'100%'}}/>
+                </Row>: null
+            }
+                <Row>
+                    <h3>Inwon Kang</h3> 
+                    <p>
+                    Email: <a href="mailto:inwon.kang04@gmail.com" target="_blank" rel="noreferrer">inwon.kang04@gmail.com</a>
+                    </p>
+                </Row>
+                <Row>
+                    <Col>
+                    I am a current PhD student at Rensselaer Polytechnic Institute.
+                    I work mostly with Python for data/analysis oriented projects. I am also familiar with web frameworks in javascript and python for buliding quick web pages.
+                    <br/>
+                    <br/>
+                    During my undergraduate and master's program, I focused on explaininable machine learning using shallow models. 
+                    My master's project was on collecting survey data from participants and building explainable ML models to try to explain the results. 
+                    During this time, I also dabbled in NLP and built an enhanced pipeline of existing state-of-art model for <em>Preference Ellicitation</em> task using GNNs and coreference parsers.
+                    <br/>
+                    <br/>
+                    My PhD focus will be on blockchains and integration of machine learning with blockchains.
+                    {/* <br/>
+                    <br/>
+                    I like collecting datasets, one day one of them will be useful. */}
+                    </Col>
+                    <Col xs={12} className='mt-5'>
+                        <h5>Education</h5>
+                    </Col>
+                    <Col xs={12}>
+                        <ul>
+                            <li>
+                                BS in Computer Science with concentration in AI/ML (Aug 2017 - May 2021). <b>RPI</b>
+                            </li>
+                            <li>
+                                MS in Computer Science (Aug 2021 - May 2022). Supervised by Prof. <a href="https://www.cs.rpi.edu/~xial/" target="_blank" rel="noreferrer">Lirong Xia</a> <b>RPI</b>
+                            </li>
+                            <li>
+                                PhD in Computer Science (Aug 2022 - ). Supervised by Prof. <a href="https://faculty.rpi.edu/oshani-seneviratne" target="_blank" rel="noreferrer">Oshani Seneviratne</a> <b>RPI</b>
+                            </li>
+                        </ul>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+    </>
+    )
+}
 
 export default Home
